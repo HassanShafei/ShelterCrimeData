50a9a7b8fa42ac2cd12f6343ddb1b5e8e7442a18
HassanShafei <60197453+HassanShafei@users.noreply.github.com>
2023-12-22 17:35:06 -0500

    Update Streamlit_Folium.py

--- Streamlit_Folium.py
+++ Streamlit_Folium.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import folium
 from streamlit_folium import folium_static
-
+from geopy.geocoders import Nominatim
 from folium.plugins import MarkerCluster
 from streamlit_js_eval import streamlit_js_eval, get_geolocation
 
@@ -29,26 +29,26 @@ def read_data(df, num_examples=2000):
 data = read_data(df)
 
 # Button to initiate the process of getting the user's location
-st.write("Please provide your location to get the nearest service")
-if st.checkbox("Get My Location"):
+# Sidebar for user input
+st.sidebar.header("Please provide your location to get the nearest service")
+
+st.sidebar.header("User Input")
+
+
+# Select between user location and manual input
+option = st.sidebar.radio("Select Option", ["User Location", "Manual Input"])
+
+if option == "User Location":
+    # Option 1: Get user's location
     try:
         loc = get_geolocation()
         latitude_user = loc['coords']['latitude']
         longitude_user = loc['coords']['longitude']
-        st.write(f"Your coordinates are {latitude_user}, {longitude_user}")
-
-        # Slider for adjusting the circle radius
-        circle_radius = st.slider("Adjust Circle Radius (meters)", min_value=10, max_value=200, value=80)
-
-        # Plot the map
-        map = folium.Map(location=[latitude_user, longitude_user], zoom_start=12)
-        folium.Marker(
-            location=[latitude_user, longitude_user],
-            popup='Your Location',
-            icon=folium.Icon(color='red')
-        ).add_to(map)
+        st.sidebar.write(f"Your coordinates are {latitude_user}, {longitude_user}")
 
         # Draw a circle around the user's location
+        circle_radius = 80  # in meters
+        map = folium.Map(location=[latitude_user, longitude_user], zoom_start=12)
         folium.CircleMarker(
             location=[latitude_user, longitude_user],
             radius=circle_radius,
@@ -72,4 +72,46 @@ if st.checkbox("Get My Location"):
         folium_static(map, width=700, height=800)
 
     except Exception as e:
-        st.error(f"Error retrieving location: {e}")
+        st.sidebar.error(f"Error retrieving location: {e}")
+
+else:
+    # Option 2: Enter city and zip code
+    city = st.sidebar.text_input("Enter City", "")
+    zip_code = st.sidebar.text_input("Enter ZIP Code", "")
+
+    submit_button = st.sidebar.button("Submit")
+
+    if submit_button:
+        try:
+            geolocator = Nominatim(user_agent="my_geocoder")
+            location = geolocator.geocode(f"{city}, {zip_code}")
+            latitude_user, longitude_user = location.latitude, location.longitude
+            st.sidebar.write(f"Coordinates for {city}, {zip_code}: {latitude_user}, {longitude_user}")
+
+            # Draw a circle around the zip code area
+            circle_radius = 5000  # in meters
+            map = folium.Map(location=[latitude_user, longitude_user], zoom_start=12)
+            folium.Circle(
+                location=[latitude_user, longitude_user],
+                radius=circle_radius,
+                color='blue',
+                fill=True,
+                fill_color='blue',
+                fill_opacity=0.2
+            ).add_to(map)
+
+            # Create a MarkerCluster layer to group your data points
+            marker_cluster = MarkerCluster().add_to(map)
+
+            # Add data points to the map
+            for loc in data:
+                location = loc['latitude'], loc['longitude']
+                popup = loc['name']
+                folium.Marker(location, popup=popup).add_to(marker_cluster)
+
+            # Render the map
+            st.header("Homeless Shelters and Crime in Philly")
+            folium_static(map, width=700, height=800)
+
+        except Exception as e:
+            st.sidebar.error(f"Error retrieving location: {e}")
